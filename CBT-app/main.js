33ba151227a5fe777ed70be063db8418ed2542ef
Gboyega Temiloluwa Marvelous <158356260+Temiloluwagboyega@users.noreply.github.com>
2024-05-21 10:15:40 +0100

    Add files via upload

---
+++ CBT-app/main.js
@@ -0,0 +1,8 @@
+function dashboard(){
+    document.getElementById('home').style.display='block'
+    document.getElementById('result').style.display='none'
+}
+function result(){
+    document.getElementById('home').style.display='none'
+    document.getElementById('result').style.display='block'
+}
